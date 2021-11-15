b6f52aac4fbdc55adecd3d49b8519efdfb8ba11d
patriottlc@ya.ru <D150889gi>
2021-11-15 19:46:31 +0300

    Initial commit

---
+++ Server/src/Server.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket server = new ServerSocket(8000);
+        System.out.println("Server started");
+    }
+}
